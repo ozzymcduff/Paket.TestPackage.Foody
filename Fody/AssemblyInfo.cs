3274346aeaa8cdfba32ab34f9c654b84b4c511b5
Oskar Gewalli <gewalli@gmail.com>
2016-05-09 21:32:38 +0200

    Test package

---
+++ Fody/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Paket.TestPackage.Costura")]
+[assembly: AssemblyProduct("Paket.TestPackage.Costura")]
+[assembly: AssemblyVersion("1.3.3.1")]
+[assembly: AssemblyFileVersion("1.3.3.1")]
